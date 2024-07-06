03ed72fcb033e7b87ba80f1b344bfe112c2b157e
Germain <germain.gg90@gmail.com>
2024-07-05 22:30:54 -0400

    sheets

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -22,6 +22,7 @@ const links = [
   { name: "menubar", href: "menubar" },
   { name: "input-opt", href: "input-opt" },
   { name: "progress", href: "progress" },
+  { name: "sheet", href: "sheet" },
 ].sort((a, b) => a.name.localeCompare(b.name));
 
 export default function DashboardLayout({
