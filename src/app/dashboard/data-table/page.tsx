6d5914741ff003ac2d25dcfb59223536b238fe6d
Germain <germain.gg90@gmail.com>
2024-07-07 22:17:09 -0400

    load data in component

--- src/app/dashboard/data-table/page.tsx
+++ src/app/dashboard/data-table/page.tsx
@@ -1,5 +1,13 @@
-const DataTablePage = () => {
-  return <div>DataTablePage</div>;
+import { payments } from "@/data/payments.data";
+
+const fetchData = async () => {
+  return payments;
+};
+
+const DataTablePage = async () => {
+  const data = await fetchData();
+
+  return <div></div>;
 };
 
 export default DataTablePage;
